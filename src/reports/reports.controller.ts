bba2b1dae5b192a5c787ea266d42fe499df590c7
Bruno Oliveira <bruno.oliveira@border-innovation.com>
2021-10-24 20:44:43 +0100

    140 done

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,11 @@
 import { 
     Body, 
     Controller, 
+    Get, 
     Param, 
     Patch, 
     Post, 
+    Query, 
     UseGuards 
 } from '@nestjs/common';
 import { AuthGuard } from '../guards/auth.guard';
@@ -15,6 +17,7 @@ import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -31,5 +34,10 @@ export class ReportsController {
     @UseGuards(AdminGuard)
     approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
         return this.reportsService.changeApproval(id, body.approved);
+    }
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto) {
+       // return this.reportsService.estimate();
     }
 }
